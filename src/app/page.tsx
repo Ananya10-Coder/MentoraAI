e37432395458cebcd3b022bb8a2fe2cc4502e8b0
Ananya10-Coder <ananya.gmk@gmail.com>
2026-03-29 23:43:07 +0530

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+
 import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="text-4xl font-bold text-green-500">
-      Hello world!
+    <div>
+      <Input placeholder="name" value={name} onChange={(e)=> setName(e.target.value)}/>
+      <Input placeholder="email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
+      <Input placeholder="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
+
+      <Button> Create User</Button>
     </div>
   );
 }
