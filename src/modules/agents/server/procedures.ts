3e649d215d117e49b15652c0eedb91489c027529
Ananya10-Coder <ananya.gmk@gmail.com>
2026-04-03 11:43:14 +0530

    implement data table to display agents.

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,35 +1,48 @@
+import { z } from "zod";
+import { eq, getTableColumns, sql } from "drizzle-orm";
+
 import { db } from "@/db";
-import {agents} from "@/db/schema";
-import {createTRPCRouter, baseProcedure, protectedProcedure} from "@/trpc/init";
+import { agents } from "@/db/schema";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+
 import { agentsInsertSchema } from "../schemas";
-import {z} from "zod";
-import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-    getOne: protectedProcedure.input(z.object({id: z.string()})).query(async({input}) =>{
-        const [existingAgent] = await db
-            .select()
-            .from(agents)
-            .where(eq(agents.id, input.id));
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+    const [existingAgent] = await db
+      .select({
+        // TODO: Change to actual count
+        meetingCount: sql<number>`5`,
+        ...getTableColumns(agents),
+      })
+      .from(agents)
+      .where(eq(agents.id, input.id))
 
-        return existingAgent;
-    }),
-    getMany: protectedProcedure.query(async() => {
-        const data = await db
-            .select()
-            .from(agents);
+    return existingAgent;
+  }),
+  getMany: protectedProcedure.query(async () => {
+    const data = await db
+      .select({
+        meetingCount: sql<number>`5`,
+        ...getTableColumns(agents),
+      })
+      .from(agents);
 
-        return data;
+    return data;
+  }),
+  create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agents)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+
+      return createdAgent;
     }),
-    create: protectedProcedure
-        .input(agentsInsertSchema)
-        .mutation(async({input, ctx}) => {
-            const [createdAt] = await db
-                .insert(agents)
-                .values({
-                    ...input,
-                    userId: ctx.auth.user.id,
-                })
-                .returning();
-        }),
 });
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,36 +1,46 @@
 "use client";
 
 import { useSuspenseQuery } from "@tanstack/react-query";
+
 import { useTRPC } from "@/trpc/client";
-import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
+import { EmptyState } from "@/components/empty-state";
+import { LoadingState } from "@/components/loading-state";
 
-export const AgentsView = () =>  {
-    const trpc = useTRPC();
-    const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+import { columns } from "../components/columns";
+import { DataTable } from "../components/data-table";
 
-    return(
-        <div>
-            {JSON.stringify(data, null, 2)}
-        </div>
-    )
-}
+export const AgentsView = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns} />
+      {data.length === 0 && (
+        <EmptyState
+          title="Create your first agent"
+          description="Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call."
+        />
+      )}
+    </div>
+  );
+};
 
 export const AgentsViewLoading = () => {
-    return (
-      <LoadingState
-        title="Loading Agents"
-        description="This may take a fews econds"
-      />
-    );
-  };
-  
-  export const AgentsViewError = () => {
-    return (
-      <ErrorState
-        title="Error Loading Agents"
-        description="Something went wrong"
-      />
-    )
-  }
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="This may take a fews econds"
+    />
+  );
+};
+
+export const AgentsViewError = () => {
+  return (
+    <ErrorState
+      title="Error Loading Agents"
+      description="Something went wrong"
+    />
+  )
+}
